324b7d918c40e840ede0c9062f654777cf71c548
Piyush Kumar <piyushkumarsings@gmail.com>
2024-07-01 11:12:36 +0530

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import ButtonGradient from './assets/svg/ButtonGradient'
-import Button from './components/Button'
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 const App = () => {
   return (
@@ -9,6 +8,7 @@ const App = () => {
       <h1 className='text-3xl underline'>App</h1>
       <div className="pt-[4.75rem] lg:pt-[5.25] overflow-hidden">
         <Header />
+        <Hero/>
       </div>
       <ButtonGradient />
     </>
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -11,10 +11,7 @@ const Section = ({
   return (
     <div
       id={id}
-      className={`
-      relative 
-      ${
-        customPaddings ||
+      className={`relative ${ customPaddings ||
         `py-10 lg:py-16 xl:py-20 ${crosses ? "lg:py-32 xl:py-40" : ""}`
       } 
       ${className || ""}`}
